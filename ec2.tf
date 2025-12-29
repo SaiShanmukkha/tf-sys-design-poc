f911e6b286a2db64786eb0fd50f85e5082733d47
sai <sai@rhel.lan>
2025-12-29 01:17:01 -0600

    with cloud-init

--- ec2.tf
+++ ec2.tf
@@ -4,6 +4,8 @@ resource "aws_instance" "test_tf_instance" {
   vpc_security_group_ids = [aws_security_group.sg1.id]
   key_name               = aws_key_pair.deployer_key.key_name
 
+  user_data = file("${path.module}/cloud-init.yaml")
+
   tags = {
     Name = "test_tf_instance"
   }
