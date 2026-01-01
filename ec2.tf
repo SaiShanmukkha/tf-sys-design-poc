076f0eca3405bcb8a6df3ef4143b637aef9f01a7
sai <sai@rhel.lan>
2026-01-01 08:50:03 -0600

    changed var

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,12 @@
-resource "aws_instance" "test_tf_instance" {
-  ami                    = "ami-09dcba964a9c51505"
-  instance_type          = "t3a.micro"
-  vpc_security_group_ids = [aws_security_group.sg1.id]
-  key_name               = aws_key_pair.deployer_key.key_name
+# resource "aws_instance" "test_tf_instance" {
+#   ami                    = "ami-09dcba964a9c51505"
+#   instance_type          = "t3a.micro"
+#   vpc_security_group_ids = [aws_security_group.sg1.id]
+#   key_name               = aws_key_pair.deployer_key.key_name
 
-  user_data = file("${path.module}/cloud-init.yaml")
+#   user_data = file("${path.module}/cloud-init.yaml")
 
-  tags = {
-    Name = var.instance_name
-  }
-}
+#   tags = {
+#     Name = var.instance_name
+#   }
+# }
--- iam.tf
+++ iam.tf
@@ -1,9 +1,9 @@
-resource "aws_iam_user" "test_user" {
-  count = 1
-  name  = "test_user_${count.index}"
-  tags = {
-    Environment  = "Development"
-    Time_Created = timestamp()
-    Department   = "IT"
-  }
-}
+# resource "aws_iam_user" "test_user" {
+#   count = 1
+#   name  = "test_user_${count.index}"
+#   tags = {
+#     Environment  = "Development"
+#     Time_Created = timestamp()
+#     Department   = "IT"
+#   }
+# }
