9fb2b2b9a8d05cdb98975a99af7ceb3ac48abc74
sai <sai@rhel.lan>
2026-01-01 08:54:02 -0600

    fixed errors

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,1 @@
-output "ec2_public_ip" {
-  description = "The public IP address of the EC2 instance"
-  value       = aws_instance.test_tf_instance.public_ip
-}
 
-output "ec2_dns_name" {
-  description = "The public DNS name of the EC2 instance"
-  value       = aws_instance.test_tf_instance.public_dns
-}
